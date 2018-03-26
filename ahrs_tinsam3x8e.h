aed0348ca7a8ef4f51749cc1992999d7ba65fa00
Tinnakon Kheowree <tinnakonza@gmail.com>
2018-03-26 10:07:11 +0700

    Update ahrs_tinsam3x8e.h

--- ahrs_tinsam3x8e.h
+++ ahrs_tinsam3x8e.h
@@ -16,24 +16,24 @@ https://www.facebook.com/tinnakonza
 float exInt , eyInt , ezInt ;	// scaled integral error
 float q0, q1 , q2 , q3 ;	// quaternion elements representing the estimated orientation
 float ahrs_p,ahrs_r,ahrs_y;
-float Heading = 0.0;
-float setHeading = 0.0;
-float previousEx = 0.0;
-float previousEy = 0.0;
-float previousEz = 0.0;
+float Heading = 0.0f;
+float setHeading = 0.0f;
+float previousEx = 0.0f;
+float previousEy = 0.0f;
+float previousEz = 0.0f;
 
 void ahrs_initialize()
 {
-  exInt=0.0;
-  eyInt=0.0;
-  ezInt=0.0;
-  q0=1.0;
-  q1=0.0;
-  q2=0.0;
-  q3=0.0;
+  exInt=0.0f;
+  eyInt=0.0f;
+  ezInt=0.0f;
+  q0=1.0f;
+  q1=0.0f;
+  q2=0.0f;
+  q3=0.0f;
 }
 boolean isSwitched(float previousError, float currentError) {
-  if ((previousError > 0.0 &&  currentError < 0.0) || (previousError < 0.0 &&  currentError > 0.0)) {
+  if ((previousError > 0.0f &&  currentError < 0.0f) || (previousError < 0.0f &&  currentError > 0.0f)) {
     return true;
   }
   return false;
@@ -43,7 +43,7 @@ void ahrs_updateMARG(float gx, float gy, float gz, float ax, float ay, float az,
   float hx, hy, hz, bx, bz;
   float vx, vy, vz, wx, wy, wz;
   float ex, ey, ez, ezMag;
-  halfT=G_Dt/2.0;        
+  halfT=G_Dt/2.0f;        
   norm1 = sqrt(ax*ax + ay*ay + az*az); // normalise the measurements    
   ax = ax / norm1;
   ay = ay / norm1;
@@ -75,18 +75,18 @@ void ahrs_updateMARG(float gx, float gy, float gz, float ax, float ay, float az,
   //*
   exInt += ex*Ki;
     if (isSwitched(previousEx,ex)) {
-    exInt = 0.0;
+    exInt = 0.0f;
   }
   previousEx = ex;
   eyInt += ey*Ki;
     if (isSwitched(previousEy,ey)) {
-    eyInt = 0.0;
+    eyInt = 0.0f;
   }
   previousEy = ey;
   float ezi = (ez*Ki + ezMag*Ki_mag);
     ezInt += ezi;
     if (isSwitched(previousEz,ezi)) {
-    ezInt = 0.0;
+    ezInt = 0.0f;
   }
   previousEz = ezi;
   gx += (Kp*ex + exInt);                   // adjusted gyroscope measurements
@@ -112,15 +112,15 @@ void ahrs_updateMARG(float gx, float gy, float gz, float ax, float ay, float az,
   float q2q3 = q2*q3;
   float q3q3 = q3*q3;  
   //direction cosine matrix (DCM), Rotation matrix , Rotated Frame to Stationary Frame XYZ  ,, Quaternions_and_spatial_rotation
-  DCM00 = 2*(0.5 - q2q2 - q3q3);//2*(0.5 - q2q2 - q3q3);//=q0q0 + q1q1 - q2q2 - q3q3
-  DCM01 = 2*(q1q2 - q0q3);//2*(q0q1 + q2q3)
-  DCM02 = 2*(q1q3 + q0q2);//2*(q1q3 - q0q2); 2*(q0q2 - q1q3)
-  DCM10 = 2*(q1q2 + q0q3);
-  DCM11 = 2*(0.5 - q1q1 - q3q3);//2*(0.5 - q1q1 - q3q3);//q0q0 - q1q1 + q2q2 - q3q3
-  DCM12 = 2*(q2q3 - q0q1);
-  DCM20 = 2*(q1q3 - q0q2);//-sin pitch
-  DCM21 = 2*(q2q3 + q0q1);//sin r *cos p
-  DCM22 = 2*(0.5 - q1q1 - q2q2);//cos r*cosp ,,2*(0.5 - q1q1 - q2q2);//=q0q0 - q1q1 - q2q2 + q3q3
+  DCM00 = 2.0f*(0.5f - q2q2 - q3q3);//2*(0.5 - q2q2 - q3q3);//=q0q0 + q1q1 - q2q2 - q3q3
+  DCM01 = 2.0f*(q1q2 - q0q3);//2*(q0q1 + q2q3)
+  DCM02 = 2.0f*(q1q3 + q0q2);//2*(q1q3 - q0q2); 2*(q0q2 - q1q3)
+  DCM10 = 2.0f*(q1q2 + q0q3);
+  DCM11 = 2.0f*(0.5f - q1q1 - q3q3);//2*(0.5 - q1q1 - q3q3);//q0q0 - q1q1 + q2q2 - q3q3
+  DCM12 = 2.0f*(q2q3 - q0q1);
+  DCM20 = 2.0f*(q1q3 - q0q2);//-sin pitch
+  DCM21 = 2.0f*(q2q3 + q0q1);//sin r *cos p
+  DCM22 = 2.0f*(0.5f - q1q1 - q2q2);//cos r*cosp ,,2*(0.5 - q1q1 - q2q2);//=q0q0 - q1q1 - q2q2 + q3q3
   //DCM23 = q0*q0 - q1*q1 + q2*q2 - q3*q3;
   //ahrs_toEuler();
   //Quaternion to Euler Angles Conversion
@@ -132,33 +132,33 @@ void ahrs_updateMARG(float gx, float gy, float gz, float ax, float ay, float az,
   //float cosroll = cos(ahrs_r);
   //float sinroll = sin(ahrs_r);
   //float tanpitch = tan(ahrs_p);
-  if(cos_rollcos_pitch <= 0.866025403)//30 deg = 0.866025403
+  if(cos_rollcos_pitch <= 0.866025403f)//30 deg = 0.866025403
     {
-      cos_rollcos_pitch = 0.866025403;
+      cos_rollcos_pitch = 0.866025403f;
     }
 ////transition between 0 and 360 or -180 and 180 ,By aeroquad//////////    
      Heading = ahrs_y*RAD_TO_DEG - setHeading;
-    if (ahrs_y*RAD_TO_DEG <= (setHeading - 180)) {
-      Heading += 360;
+    if (ahrs_y*RAD_TO_DEG <= (setHeading - 180.0f)){
+      Heading += 360.0f;
     }
-    if (ahrs_y*RAD_TO_DEG >= (setHeading + 180)) {
-      Heading -= 360;
-    } 
+    if (ahrs_y*RAD_TO_DEG >= (setHeading + 180.0f)){
+      Heading -= 360.0f;
+    }
 //////Earth Frame///////////////
-accrX_Earthf = accrX_Earthf + (AccXf - accrX_Earthf)*G_Dt*42.51245;//22.51245 42.5 18.5 Low pass filter ,smoothing factor  α := dt / (RC + dt)
-accrY_Earthf = accrY_Earthf + (AccYf - accrY_Earthf)*G_Dt*42.51245;//12.51245 
-accrZ_Earthf = accrZ_Earthf + (AccZf - accrZ_Earthf)*G_Dt*42.51245;//25.51245
-accrX_Earth = (accrX_Earthf*DCM00 + accrY_Earthf*DCM01 + accrZ_Earthf*DCM02)*-1.0;
-accrY_Earth = (accrX_Earthf*DCM10 + accrY_Earthf*DCM11 + accrZ_Earthf*DCM12)*-1.0;
+accrX_Earthf = accrX_Earthf + (AccXf - accrX_Earthf)*G_Dt*42.51245f;//22.51245 42.5 18.5 Low pass filter ,smoothing factor  α := dt / (RC + dt)
+accrY_Earthf = accrY_Earthf + (AccYf - accrY_Earthf)*G_Dt*42.51245f;//12.51245 
+accrZ_Earthf = accrZ_Earthf + (AccZf - accrZ_Earthf)*G_Dt*42.51245f;//25.51245
+accrX_Earth = (accrX_Earthf*DCM00 + accrY_Earthf*DCM01 + accrZ_Earthf*DCM02)*-1.0f;
+accrY_Earth = (accrX_Earthf*DCM10 + accrY_Earthf*DCM11 + accrZ_Earthf*DCM12)*-1.0f;
 accrZ_Earth = (accrX_Earthf*DCM20 + accrY_Earthf*DCM21 + accrZ_Earthf*DCM22) - acc_offsetZ2;//acc_offsetZ2
 
-applyDeadband(accrX_Earth, 0.02);//+-0.03 m/s^2
-applyDeadband(accrY_Earth, 0.02);//+-0.03 m/s^2
-applyDeadband(accrZ_Earth, 0.02);//+-0.03 m/s^2
+applyDeadband(accrX_Earth, 0.02f);//+-0.03 m/s^2
+applyDeadband(accrY_Earth, 0.02f);//+-0.03 m/s^2
+applyDeadband(accrZ_Earth, 0.02f);//+-0.03 m/s^2
 float gyroX_Earthff = (GyroXf*DCM00 + GyroYf*DCM01 + GyroZf*DCM02);//rad/s
 float gyroY_Earthff = (GyroXf*DCM10 + GyroYf*DCM11 + GyroZf*DCM12);
 float gyroZ_Earthff = (GyroXf*DCM20 + GyroYf*DCM21 + GyroZf*DCM22);
-gyroX_Earth = gyroX_Earth + (gyroX_Earthff - gyroX_Earth)*G_Dt*12.51;//32.51
-gyroY_Earth = gyroY_Earth + (gyroY_Earthff - gyroY_Earth)*G_Dt*12.51;
-gyroZ_Earth = gyroZ_Earth + (gyroZ_Earthff - gyroZ_Earth)*G_Dt*12.51;
+gyroX_Earth = gyroX_Earth + (gyroX_Earthff - gyroX_Earth)*G_Dt*12.51f;//32.51
+gyroY_Earth = gyroY_Earth + (gyroY_Earthff - gyroY_Earth)*G_Dt*12.51f;
+gyroZ_Earth = gyroZ_Earth + (gyroZ_Earthff - gyroZ_Earth)*G_Dt*12.51f;
 }
