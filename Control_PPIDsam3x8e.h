8508dfdb5f8c44b81f9c4082aabad4b7e0c0db01
Tinnakon Kheowree <tinnakonza@gmail.com>
2018-03-26 10:04:11 +0700

    Update Control_PPIDsam3x8e.h

--- Control_PPIDsam3x8e.h
+++ Control_PPIDsam3x8e.h
@@ -42,95 +42,95 @@ uint8_t endAuto = 0;
 
 void Control_PPIDRate(){
 // ROLL CONTROL P-PID  control  By tinnakon///////////
-  float setpoint_roll = ((CH_AILf-CH_AIL_Cal)*0.085);//0.12 max +-45 deg  ////+-18  + Control_YBf
-  applyDeadband(setpoint_roll, 2.5);//2.5 ,1.2
+  float setpoint_roll = ((CH_AILf-CH_AIL_Cal)*0.085f);//0.12 max +-45 deg  ////+-18  + Control_YBf
+  applyDeadband(setpoint_roll, 2.5f);//2.5 ,1.2
   if(Mode == 2 || Mode == 3 && GPS_FIX  == 1){
     setpoint_roll = Control_YBf;
   }
-  setpoint_rate_roll = (0.095*setpoint_rate_roll/(0.095+G_Dt)) + ((setpoint_roll-setpoint_rollold)/(0.095+G_Dt));//0.065 ,Diff remote
+  setpoint_rate_roll = (0.095f*setpoint_rate_roll/(0.095f+G_Dt)) + ((setpoint_roll-setpoint_rollold)/(0.095f+G_Dt));//0.065 ,Diff remote
   setpoint_rollold = setpoint_roll;
-  setpoint_rate_roll = constrain(setpoint_rate_roll, -80, 80);//+-80 deg/s
+  setpoint_rate_roll = constrain(setpoint_rate_roll, -80.0f, 80.0f);//+-80 deg/s
   float error_roll = setpoint_roll - (ahrs_r*RAD_TO_DEG);//ahrs_r*ToDeg
   float error_rate_roll = setpoint_rate_roll + error_roll*Kp_levelRoll  - GyroXf*RAD_TO_DEG;
   roll_I_rate += error_rate_roll*Ki_rateRoll*G_Dt;
-  roll_I_rate = constrain(roll_I_rate, -5, 5);//5 ,+-150
+  roll_I_rate = constrain(roll_I_rate, -5.0f, 5.0f);//5 ,+-150
   roll_D_rate = (tar*roll_D_rate/(tar+G_Dt)) + ((error_rate_roll-error_rate_rollold)/(tar+G_Dt));
   error_rate_rollold = error_rate_roll;
   u_roll = Kp_rateRoll*error_rate_roll + roll_I_rate + Kd_rateRoll*roll_D_rate;
-  u_roll = constrain(u_roll, -280, 280);//+-250 +-300 120
+  u_roll = constrain(u_roll, -280.0f, 280.0f);//+-250 +-300 120
 // PITCH CONTROL P-PID  control  By tinnakon///////////
-  float setpoint_pitch = ((CH_ELEf-CH_ELE_Cal)*-0.085);//max +-45 deg  ////+-18 - Control_XBf
-  applyDeadband(setpoint_pitch, 2.5);//1.2
+  float setpoint_pitch = ((CH_ELEf-CH_ELE_Cal)*-0.085f);//max +-45 deg  ////+-18 - Control_XBf
+  applyDeadband(setpoint_pitch, 2.5f);//1.2
     if(Mode == 2 || Mode == 3 && GPS_FIX  == 1){
     setpoint_pitch = Control_XBf;
   }
-  setpoint_rate_pitch = (0.095*setpoint_rate_pitch/(0.095+G_Dt)) + ((setpoint_pitch-setpoint_pitchold)/(0.095+G_Dt));//0.065 Diff remote
+  setpoint_rate_pitch = (0.095f*setpoint_rate_pitch/(0.095f+G_Dt)) + ((setpoint_pitch-setpoint_pitchold)/(0.095f+G_Dt));//0.065 Diff remote
   setpoint_pitchold = setpoint_pitch;
-  setpoint_rate_pitch = constrain(setpoint_rate_pitch, -80, 80);//+-80
+  setpoint_rate_pitch = constrain(setpoint_rate_pitch, -80.0f, 80.0f);//+-80 deg/s
   float error_pitch = setpoint_pitch - (ahrs_p*RAD_TO_DEG);//ahrs_p*RAD_TO_DEG
   float error_rate_pitch = setpoint_rate_pitch + error_pitch*Kp_levelPitch - GyroYf*RAD_TO_DEG;
   pitch_I_rate += error_rate_pitch*Ki_ratePitch*G_Dt;
-  pitch_I_rate = constrain(pitch_I_rate, -5, 5);//+-150
+  pitch_I_rate = constrain(pitch_I_rate, -5.0f, 5.0f);//+-150
   pitch_D_rate = (tar*pitch_D_rate/(tar+G_Dt)) + ((error_rate_pitch-error_rate_pitchold)/(tar+G_Dt));
   error_rate_pitchold = error_rate_pitch;
   u_pitch = Kp_ratePitch*error_rate_pitch + pitch_I_rate + Kd_ratePitch*pitch_D_rate;
-  u_pitch = constrain(u_pitch, -280, 280);//+-250 +-300 120
+  u_pitch = constrain(u_pitch, -280.0f, 280.0f);//+-250 +-300 120
 // YAW  CONTROL P-PID  control  By tinnakon///////////
-float setpoint_rate_yaw = (CH_RUDf-CH_RUD_Cal)*0.55;//0.4 0.35
-  applyDeadband(setpoint_rate_yaw, 8.5);//6.5
-  if(abs(setpoint_rate_yaw) > 0.1){
+float setpoint_rate_yaw = (CH_RUDf-CH_RUD_Cal)*0.42f;//0.55 0.4 0.35
+  applyDeadband(setpoint_rate_yaw, 6.3f);//6.5
+  if(abs(setpoint_rate_yaw) > 0.1f){
    setHeading = ahrs_y*RAD_TO_DEG;// 0 degree ,ahrs_tin.h
   }
-  float error_yaw = 0.0 - Heading;
+  float error_yaw = 0.0f - Heading;
   float error_rate_yaw = setpoint_rate_yaw + error_yaw*Kp_levelyaw - GyroZf*RAD_TO_DEG;
   yaw_I_rate += error_rate_yaw*Ki_rateYaw*G_Dt;
-  yaw_I_rate = constrain(yaw_I_rate, -120, 120);//+-120
+  yaw_I_rate = constrain(yaw_I_rate, -120.0f, 120.0f);//+-120
   yaw_D_rate = (tar*yaw_D_rate/(tar+G_Dt)) + ((error_rate_yaw-error_rate_yawold)/(tar+G_Dt));//0.095
   error_rate_yawold = error_rate_yaw;
   u_yaw = Kp_rateYaw*error_rate_yaw + yaw_I_rate + Kd_rateYaw*yaw_D_rate;
-  u_yaw = constrain(u_yaw, -340, 340);//+-340 +-170 +-150
+  u_yaw = constrain(u_yaw, -360.0f, 360.0f);//+-340 +-170 +-150
  ////Altitude//////////////////////////////////////////////////////////////////////////////////////////////////////  
- Velocity_THR = (CH_THRf - 1500)*0.00469;//0.00469 +- 2.1 m/s
- applyDeadband(Velocity_THR, 0.23);//0.23 0.17
+ Velocity_THR = (CH_THRf - 1500.0f)*0.00369f;//0.00469 +- 2.1 m/s
+ applyDeadband(Velocity_THR, 0.23f);//0.23 0.17
   if(Mode == 1 || Mode == 2)//Altitude Hold, 
   {
     Altitude_Hold = Altitude_Hold + (Velocity_THR*G_Dt);//Integral Velocity_THR
-    Altitude_Hold = constrain(Altitude_Hold, 0.0, Altitude_Max);//0 - 10 m
+    Altitude_Hold = constrain(Altitude_Hold, 0.0f, Altitude_Max);//0 - 10 m
     err_hz = Altitude_Hold - z1_hat;
-    err_hz = constrain(err_hz, -1.0, 1.0);//+-2 m
-    float error_Vz = Velocity_THR*0.85 - z2_hat;
+    err_hz = constrain(err_hz, -1.0f, 1.0f);//+-2 m
+    float error_Vz = Velocity_THR*0.3015f - z2_hat;//0.32 0.55
     hz_D_rate = (tar*hz_D_rate/(tar+G_Dt)) + ((error_Vz - error_Vz_old)/(tar+G_Dt));// D Controller
     error_Vz_old = error_Vz;
    // float error_hII = err_hz*1.25 + error_Vz;//0.55
     hz_I = hz_I + (Ki_altitude*err_hz*G_Dt);
-    hz_I = constrain(hz_I, -100, 600);//+-600
+    hz_I = constrain(hz_I, -100.0f, 600.0f);//+-600
     float uthrottle2 = (err_hz*Kp_altitude) + hz_I + (error_Vz*Kd_altitude) + (hz_D_rate*Kd2_altitude) - (z3_hat*Ka_altitude);//state feedback
     //uthrottle = uthrottle2*m_quad/(cos_roll*cos_pitch);// = u*m*d/b*(cosr*cosp)     ,m*d/b = 122.33  (cos_roll*cos_pitch)
-    uthrottle = uthrottle2;//  /cos_rollcos_pitch
+    uthrottle = uthrottle2*m_quad;//  /cos_rollcos_pitch
   }
   else if(Mode == 3)//Automatic  Takeoff, Landing
   {
     err_hz = h_counter - z1_hat;
-    err_hz = constrain(err_hz, -1.0, 1.0);//+-2 m
+    err_hz = constrain(err_hz, -1.0f, 1.0f);//+-2 m
     float error_Vz = Vz_Hold - z2_hat;//Vz_Hold 
     hz_D_rate = (tar*hz_D_rate/(tar+G_Dt)) + ((error_Vz - error_Vz_old)/(tar+G_Dt));// D Controller
     error_Vz_old = error_Vz;
     //float error_hII = err_hz*1.25 + error_Vz;//0.55
     hz_I = hz_I + (Ki_altitude*err_hz*G_Dt);//1.9 - 2.9  18.5  22.5
-    hz_I = constrain(hz_I, -100, 600);//+-200
+    hz_I = constrain(hz_I, -100.0f, 600.0f);//+-200
     float uthrottle2 = (err_hz*Kp_altitude) + hz_I + (error_Vz*Kd_altitude) + (hz_D_rate*Kd2_altitude) - (z3_hat*Ka_altitude);//state feedback
     //uthrottle = uthrottle2*m_quad/(cos_roll*cos_pitch);// = u*m*d/b*(cosr*cosp)     ,m*d/b = 122.33  (cos_roll*cos_pitch)
-    uthrottle = uthrottle2;//  /cos_rollcos_pitch
+    uthrottle = uthrottle2*m_quad;//  /cos_rollcos_pitch
   }
   else
   {
-    hz_I = 0.0;
-    uthrottle = 0.0;
+    hz_I = 0.0f;
+    uthrottle = 0.0f;
     Altitude_Hold = z1_hat;
     I_throttle = CH_THRf;
   }//end Altitude Hold
 //uthrottle = constrain(uthrottle, -200, 200);//+-120 +-150
-uAltitude = I_throttle + uthrottle;//m*g = 10.8 N = 
+uAltitude = (I_throttle + uthrottle)/cos_rollcos_pitch;//m*g = 10.8 N = 
 uAltitude = constrain(uAltitude, MINCOMMAND, MAXTHROTTLE);
 }
 /////////////////////////////
@@ -199,7 +199,7 @@ void Automatictakeland(){
       target_LON = waypoint1_LON;
       Status_waypoint = 1;
     }
-     if(time_auto > 8 && abs(error_LAT) <= 70 && abs(error_LON) <= 70 && endAuto == 1 && Status_waypoint == 1)//50 10 Landing and position hold mode
+     if(time_auto > 8 && abs(error_LAT) <= 70.0f && abs(error_LON) <= 70.0f && endAuto == 1 && Status_waypoint == 1)//50 10 Landing and position hold mode
     {
       timeLanding++;
       if(timeLanding >= 30)//relay 3 s Landing
@@ -214,25 +214,30 @@ void Automatictakeland(){
     timeLanding = 0;
     timeOff = 0;
     time_auto = 0;
-    h_counter = 0.1;//0.0
-    h_counter_old = 0.1;
-    Vz_Hold = 0.0;
+    h_counter = 0.1f;//0.0
+    h_counter_old = 0.1f;
+    Vz_Hold = 0.0f;
     Status_waypoint = 0;
   } 
 ////////////////////////////////////////////////////////////////// 
        if(h_counter < h_control && takeoff == 1)//take-off
       {
         endAuto = 1;
-        h_counter = h_counter + 0.042;//0.023 0.01- 0.03  0.1 = 10 s  //ramp input hz take-off
-        Vz_Hold = (h_counter - h_counter_old)/0.1;//(m/s) roop 10 Hz
+        h_counter = h_counter + 0.042f;//0.023 0.01- 0.03  0.1 = 10 s  //ramp input hz take-off
+        Vz_Hold = (h_counter - h_counter_old)/0.100050025f;//(m/s) roop 10 Hz
         h_counter_old = h_counter;
       }
        if(takeoff == 0 && endAuto == 1)//landing
       {
-        h_counter = h_counter - 0.029;//0.023 ramp input hz  landing
-        Vz_Hold = (h_counter - h_counter_old)/0.1;//(m/s) roop 10 Hz
-        h_counter_old = h_counter;
-        if(z1_hat <= 0.18)
+        h_counter = h_counter - 0.029f;//0.023 ramp input hz  landing
+        //Vz_Hold = (h_counter - h_counter_old)/0.100050025f;//(m/s) roop 10 Hz
+        Vz_Hold = -0.18f;
+        //h_counter_old = h_counter;
+         if(z1_hat <= 0.3f)
+        {
+          Vz_Hold = 0.0f;
+        }
+        if(z1_hat <= 0.18f)
         {
          endAuto = 0;
         }
@@ -251,8 +256,8 @@ void Automatictakeland(){
 ///////////////////////////////////////////////////////////////////////////////
 void Control_PositionHold(){
   if(Mode == 2 || Mode == 3 && GPS_FIX  == 1){
-          float targetRB_speedLAT = ((CH_ELEf-CH_ELE_Cal)*0.444);//vX Body Frame ,,= 200 cm/s
-          float targetRB_speedLON = ((CH_AILf-CH_AIL_Cal)*0.444);//vY Body Frame
+          float targetRB_speedLAT = ((CH_ELEf-CH_ELE_Cal)*0.624f);//vX Body Frame ,,0.444 = 200 cm/s
+          float targetRB_speedLON = ((CH_AILf-CH_AIL_Cal)*0.624f);//vY Body Frame
           applyDeadband(targetRB_speedLAT, 9.20f);//22.22 ,, +-50
           applyDeadband(targetRB_speedLON, 9.20f);//22.22
           targetRE_speedLAT = (targetRB_speedLAT*DCM00 + targetRB_speedLON*DCM01);//vX Earth Frame
@@ -265,8 +270,8 @@ void Control_PositionHold(){
           error_LON = (double)(target_LON - y1_hat)*cm_per_deg_lon;//*6371000.0 Y Error   cm
           error_LAT = constrain(error_LAT,-500.0f,500.0f);//200 = +-2 m
           error_LON = constrain(error_LON,-500.0f,500.0f);
-          float target_speedLAT = error_LAT*Kp_speed;//P Control Velocity GPS //+-200 cm/s = 2m/s
-          float target_speedLON = error_LON*Kp_speed;//P Control Velocity GPS
+          float target_speedLAT = (error_LAT*Kp_speed) + (targetRE_speedLAT*0.312f);//P Control Velocity GPS //+-200 cm/s = 2m/s
+          float target_speedLON = (error_LON*Kp_speed) + (targetRE_speedLON*0.312f);//P Control Velocity GPS
           //target_speedLAT = constrain(target_speedLAT,-200,200);//+-200 cm/s = 2m/s
           //target_speedLON = constrain(target_speedLON,-200,200);
 
@@ -275,10 +280,10 @@ void Control_PositionHold(){
 
           error_rate_LAT = constrain(error_rate_LAT,-300.0f,300.0f);//+-300 cm/s ,200
           error_rate_LON = constrain(error_rate_LON,-300.0f,300.0f);
-          GPSI_LAT = GPSI_LAT + (error_rate_LAT*Ki_gps*0.05);//5Hz=0.2 ,, 20 Hz = 0.05
-          GPSI_LON = GPSI_LON + (error_rate_LON*Ki_gps*0.05);  
-          GPSI_LAT = constrain(GPSI_LAT,-200.0f,200.0f);//win speed +-200 cm/s
-          GPSI_LON = constrain(GPSI_LON,-200.0f,200.0f);//250
+          GPSI_LAT = GPSI_LAT + (error_rate_LAT*Ki_gps*0.05f);//5Hz=0.2 ,, 20 Hz = 0.05
+          GPSI_LON = GPSI_LON + (error_rate_LON*Ki_gps*0.05f);  
+          GPSI_LAT = constrain(GPSI_LAT,-300.0f,300.0f);//win speed +-200 cm/s
+          GPSI_LON = constrain(GPSI_LON,-300.0f,300.0f);//300
           //Control_XEf = error_rate_LAT*Kd_gps;//P Control speed 
           //Control_YEf = error_rate_LON*Kd_gps;
           Control_XEf = error_LAT*Kp_gps + error_rate_LAT*Kd_gps + GPSI_LAT;//PID Control speed 
@@ -288,7 +293,7 @@ void Control_PositionHold(){
           //The desired roll and pitch angles by tinnakon 
           float urolldesir = (Control_YEf*m_quad)/uAltitude;//uAltitude/2 = 1000 - 1900
           float upitchdesir = (Control_XEf*m_quad*-1.0f)/uAltitude;//*-1
-          urolldesir = constrain(urolldesir,-0.7f,0.7f);//+-0.7 = +-44.427
+          urolldesir = constrain(urolldesir,-0.7f,0.7f);//+-0.7 = +-40.1 deg
           upitchdesir = constrain(upitchdesir,-0.7f,0.7f);
           float temp_YBf = asin(urolldesir)*RAD_TO_DEG;//Control Earth Frame
           float temp_XBf = asin(upitchdesir)*RAD_TO_DEG;//Control Earth Frame
@@ -321,13 +326,19 @@ void Control_PositionHold(){
 void Cal_GPS(){
 //GPS_LAT1 = GPS_coordLAT*1e-7;///10000000.0 1e-7 degrees / position as degrees (*10E7)
 //GPS_LON1 = GPS_coordLON*1e-7;
-velocity_northff = velocity_north + (gyroY_Earth*arm_GPS);//+ (gyroY_Earth*arm_GPS) velocity GPS Rotated Frame of arm gps
-velocity_eastff = velocity_east - (gyroX_Earth*arm_GPS);//- (gyroX_Earth*arm_GPS)
+//velocity_northff = velocity_north + (gyroY_Earth*arm_GPS);//+ (gyroY_Earth*arm_GPS) velocity GPS Rotated Frame of arm gps
+//velocity_eastff = velocity_east - (gyroX_Earth*arm_GPS);//- (gyroX_Earth*arm_GPS)
 //Expressing latitude and longitude as linear units Earth's average meridional radius is 6367449 m
 ////https://en.wikipedia.org/wiki/Geographic_coordinate_system ///mn
-float latMid = DEG_TO_RAD*GPS_coordLAT*1e-7;
+float latMid = DEG_TO_RAD*x1_hat;
 cm_per_deg_lat = (111132.92 - 559.82*cos(2*latMid) + 1.175*cos(4*latMid))*100.0d;
 cm_per_deg_lon = (111412.84*cos(latMid) - 93.5*cos(3*latMid))*100.0d;
+////////////////////////////////////////////////////////////////////////////////////
+float lonMid = DEG_TO_RAD*y1_hat;
+float lat2 = DEG_TO_RAD*GPS_LAT_HOME;//target_LAT
+float lon2 = DEG_TO_RAD*GPS_LON_HOME;//target_LON
+float a=atan2(sin(lon2-lonMid)*cos(lat2), cos(latMid)*sin(lat2)-sin(latMid)*cos(lat2)*cos(lon2-lonMid));
+yaw_bearing = a*RAD_TO_DEG;
   /*
   if(GPS_FIX  == 1){
  //Apply moving average filter to GPS data
